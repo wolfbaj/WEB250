9bb94880becbebcd131f71ac3ba47299b6a6ac1a
wolfbaj <98982452+wolfbaj@users.noreply.github.com>
2024-04-10 16:39:14 -0400

    Update home.php

--- contents/home.php
+++ contents/home.php
@@ -2,4 +2,4 @@
 <p class="alignLeftMain">this site is as of the spring 2024 semester still a work in progress.</p>
 <p class="alignLeftMain">In the mean time feel free to browse what I have finshed using the navbar above.</p>
 <p>to learn a bit more about me here is my introduction:</p>
-<a href="index.php?page=introduction">About me</a>
+<a href="index.php?page=introduction" class="clickable">About me</a>
